63e4dd345abe8978a4f62a30d580a28384068101
Tuyenkaka2005 <nguyendangtuyen2005@gmail.com>
2026-05-13 01:43:21 -0700

    Initial Commit

---
+++ AuthFlow/AuthFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthFlowApp.swift
+//  AuthFlow
+//
+//  Created by Tuyen on 13/5/26.
+//
+
+import SwiftUI
+
+@main
+struct AuthFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
